ed9dcdd49b4a33de3e0c517750f51f72402b585d
小潘 <1141841975@qq.com>
2025-08-21 15:16:45 +0800

    QA-验证码的保存这边存在了数据库，后面可以改进存入redis

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, render_template
-from exts import mail
+from flask import Blueprint, render_template, jsonify
+from exts import mail, db
 from flask_mail import Message
 from flask import request
 import string
 import random
+from models import EmailCaptchaModel
 
 # 都要以 /auth开头
 bp = Blueprint("auth", __name__, url_prefix="/auth")
@@ -22,6 +23,7 @@ def register():
 
 @bp.route("/captcha/email")
 def get_email_captcha():
+    # 发送邮箱获得验证码
     email = request.args.get("email")
     source = string.digits*4
     captcha = random.sample(source, 4)
@@ -29,9 +31,13 @@ def get_email_captcha():
     message = Message(subject="注册验证码", recipients=[email], body=f"您的验证码是:{captcha}")
     mail.send(message)
     # 用数据库方式存储
-
-    #用redis去存储
-    return "success"
+    email_captcha = EmailCaptchaModel(email=email, captcha=captcha)
+    db.session.add(email_captcha)
+    db.session.commit()
+    # 用redis去存储
+    # RESTful API
+    # {code:200/400/500, message:"", data:{}}
+    return jsonify({"code": 200, "message": "", "data": None})
 
 
 @bp.route("/mail/test")
--- models.py
+++ models.py
@@ -1,6 +1,10 @@
 from exts import db
 from datetime import datetime
 
+# flask db init: 只需要执行一次
+# flask db migrate: 将orm模型生成迁移成脚本
+# flask db upgrade: 将迁移脚本映射到数据库中
+
 
 class UserModel(db.Model):
     __tablename__ = 'users'
@@ -9,3 +13,10 @@ class UserModel(db.Model):
     password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
     join_time = db.Column(db.DateTime, default=datetime.now)
+
+
+class EmailCaptchaModel(db.Model):
+    __tablename__ = 'email_captcha'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    captcha = db.Column(db.String(100), nullable=False)
