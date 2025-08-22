2215c8074021ba29ae4cb81e6d513db8e0900ce8
小潘 <1141841975@qq.com>
2025-08-22 08:40:27 +0800

    QA-发布页面渲染

--- blueprints/qa.py
+++ blueprints/qa.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, render_template
 
 bp = Blueprint('qa', __name__, url_prefix='/')
 
@@ -6,3 +6,11 @@ bp = Blueprint('qa', __name__, url_prefix='/')
 @bp.route('/')
 def index():
     return "欢迎来到问答平台首页"
+
+
+# GET:渲染模板
+# POST:把数据存储到数据库
+@bp.route('/qa/public', methods=['GET', 'POST'])
+def public_qa():
+    if request.method == 'GET':
+        return render_template("public_question.html")
